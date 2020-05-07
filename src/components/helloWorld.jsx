29f1b99c2486372dc3b82ad62f9907552a591798
gifgif1995 <ryan.giffin1000@gmail.com>
2020-05-07 10:00:05 -0600

    First Commit

---
+++ src/components/helloWorld.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+class HelloWorld extends React.Component {
+    render() {
+        return (
+        <>
+        <h3>Things I need to do:</h3>
+            <ul>
+                <li>Learn React</li>
+                <li>Climb Mt. Everest</li>
+                <li>Run a Marathon</li>
+                <li>Feed the Cat</li>
+            </ul> 
+            </>
+        
+        )
+    }
+}
+
+export default HelloWorld;
